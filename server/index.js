8d55953f182e15af3f7998d8e4a7955bb5b08eb0
Steph Barker <sbarkerdrums@gmail.com>
2022-04-18 17:46:05 -0700

    adds routes, endpoints and mock data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,42 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Routes
+
+app.get("/v1/google-status", (req, res) => {
+    res.json({
+    "url": "https://www.google.com",
+    "statusCode": 200,
+    "duration": 154,
+    "date": 1606636800 });
+  });
+
+  app.get("/v1/amazon-status", (req, res) => {
+    res.json({
+    "url": "https://www.amazon.com",
+    "statusCode": 200,
+    "duration": 154,
+    "date": 1606636800 });
+  });
+
+  app.get("/v1/all-status", (req, res) => {
+    res.json([
+        {
+            "url": "https://www.google.com",
+            "statusCode": 200,
+            "duration": 150,
+            "date": 1606636800
+        },
+        {
+            "url": "https://www.amazon.com",
+            "statusCode": 200,
+            "duration": 273,
+            "date": 1606636803
+        }
+    ]);
+  });
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
