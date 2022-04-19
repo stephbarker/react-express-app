b06dcc376b9cb52ac5c48a71ed555525d809c39f
Steph Barker <sbarkerdrums@gmail.com>
2022-04-18 21:10:23 -0700

    displays list data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,25 @@ import React from 'react'
 // import Google from './Google';
 // import Amazon from './Amazon';
 
-export default function Home() {
-    <div>
-        <h1>Status List</h1>
-        {/* <Google />
-        <Amazon /> */}
-    </div>
+export default function Home({ statusData }) {
+    return (
+        <div>
+            {statusData.map((item) => {
+                const { url, statusCode, duration, date } = item;
+                return (
+                    <article>
+                        <h3>{url}</h3>
+                        <p>
+                        {statusCode}
+                         <br/>
+                         {duration}
+                         <br/>
+                         {date}
+                        </p>
+                    </article>
+                );
+            })}   
+        </div>
+    );
 };
+
